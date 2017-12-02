ad82352e9d7f31aabc540116d74df81d2c779238
Ola Christian Gundelsby <lug00ber@gmail.com>
2017-12-02 16:03:04 +0100

    Day 1, part 2

--- 01/src/solver/solver.js
+++ 01/src/solver/solver.js
@@ -1,21 +1,52 @@
-/**
- * The captcha requires you to review a sequence of digits (your puzzle input)
- * and find the sum of all digits that match the next digit in the list. The
- * list is circular, so the digit after the last digit is the first digit in
- * the list.
- */
-module.exports = (input) => {
-  const characters = input.split('')
-  const length = characters.length
-  const first = characters[0]
-  const matchingDigits = characters.filter((digit, index) => {
-    const nextDigit = index + 1 < length ? characters[index + 1] : first
-
-    return digit === nextDigit
-  })
-  const result = matchingDigits.reduce((sum, digit) => {
+function sum (digits) {
+  return digits.reduce((sum, digit) => {
     return sum + Number.parseInt(digit)
   }, 0)
+}
 
-  return String(result)
+function massage (data) {
+  const characters = data.split('')
+  return {
+    characters,
+    length: characters.length
+  }
+}
+
+module.exports = {
+  /**
+   * The captcha requires you to review a sequence of digits (your puzzle input)
+   * and find the sum of all digits that match the next digit in the list. The
+   * list is circular, so the digit after the last digit is the first digit in
+   * the list.
+   */
+  sequential: (input) => {
+    const {characters, length} = massage(input)
+    const first = characters[0]
+    const matchingDigits = characters.filter((digit, index) => {
+      const nextDigit = index + 1 < length ? characters[index + 1] : first
+
+      return digit === nextDigit
+    })
+    const result = sum(matchingDigits)
+
+    return String(result)
+  },
+  /**
+   * Now, instead of considering the next digit, it wants you to
+   * consider the digit halfway around the circular list. That
+   * is, if your list contains 10 items, only include a digit in
+   * your sum if the digit 10/2 = 5 steps forward matches it.
+   * Fortunately, your list has an even number of elements.
+  */
+  halfway: (input) => {
+    const {characters, length} = massage(input)
+    const offset = length / 2
+    const matchingDigits = characters.filter((digit, index) => {
+      const halfwayIndex = index - offset < 0 ? index + offset : index - offset
+      return digit === characters[halfwayIndex]
+    })
+    const result = sum(matchingDigits)
+
+    return String(result)
+  }
 }
--- 01/test/solver/solver-test.js
+++ 01/test/solver/solver-test.js
@@ -1,49 +1,95 @@
-/**
- * The captcha requires you to review a sequence of digits (your puzzle input)
- * and find the sum of all digits that match the next digit in the list. The
- * list is circular, so the digit after the last digit is the first digit in
- * the list.
- */
 const solver = require('../../src/solver/solver')
-const {assert, refute} = require('../test-helper')
+const { assert, refute } = require('../test-helper')
 
 describe('Solver', () => {
   /**
-   * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the
-   * second digit and the third digit (2) matches the fourth digit.
+   * The captcha requires you to review a sequence of digits (your puzzle input)
+   * and find the sum of all digits that match the next digit in the list. The
+   * list is circular, so the digit after the last digit is the first digit in
+   * the list.
    */
-  it('Should produce 3 from 1122', () => {
-    const input = '1122'
-    const output = solver(input)
+  describe('Sequential', () => {
+    /**
+     * 1122 produces a sum of 3 (1 + 2) because the first digit (1) matches the
+     * second digit and the third digit (2) matches the fourth digit.
+     */
+    it('Should produce 3 from 1122', () => {
+      const input = '1122'
+      const output = solver.sequential(input)
 
-    assert.equals(output, '3')
+      assert.equals(output, '3')
+    })
+
+    /**
+     * 1111 produces 4 because each digit (all 1) matches the next.
+     */
+    it('Should produce 4 from 1111', () => {
+      const input = '1111'
+      const output = solver.sequential(input)
+
+      assert.equals(output, '4')
+    })
+    /**
+     * 1234 produces 0 because no digit matches the next.
+     */
+    it('Should produce 0 from 1234', () => {
+      const input = '1234'
+      const output = solver.sequential(input)
+
+      assert.equals(output, '0')
+    })
+    /**
+     * 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
+     */
+    it('Should produce 9 from 91212129', () => {
+      const input = '91212129'
+      const output = solver.sequential(input)
+
+      assert.equals(output, '9')
+    })
   })
 
-  /**
-   * 1111 produces 4 because each digit (all 1) matches the next.
-   */
-  it('Should produce 4 from 1111', () => {
-    const input = '1111'
-    const output = solver(input)
+  describe('Halfway', () => {
+    /**
+     * Now, instead of considering the next digit, it wants you to
+     * consider the digit halfway around the circular list. That
+     * is, if your list contains 10 items, only include a digit in
+     * your sum if the digit 10/2 = 5 steps forward matches it.
+     * Fortunately, your list has an even number of elements.
+    */
+    it('should return 6 for 1212', () => {
+      const input = '1212'
+      const output = solver.halfway(input)
 
-    assert.equals(output, '4')
-  })
-  /**
-   * 1234 produces 0 because no digit matches the next.
-   */
-  it('Should produce 0 from 1234', () => {
-    const input = '1234'
-    const output = solver(input)
+      assert.equals(output, '6')
+    })
 
-    assert.equals(output, '0')
-  })
-  /**
-   * 91212129 produces 9 because the only digit that matches the next one is the last digit, 9.
-   */
-  it('Should produce 9 from 91212129', () => {
-    const input = '91212129'
-    const output = solver(input)
+    it('should return 0 for 1221', () => {
+      const input = '1221'
+      const output = solver.halfway(input)
 
-    assert.equals(output, '9')
+      assert.equals(output, '0')
+    })
+
+    it('should return 4 for 123425', () => {
+      const input = '123425'
+      const output = solver.halfway(input)
+
+      assert.equals(output, '4')
+    })
+
+    it('should return 12 for 123123', () => {
+      const input = '123123'
+      const output = solver.halfway(input)
+
+      assert.equals(output, '12')
+    })
+
+    it('should return 4 for 12131415', () => {
+      const input = '12131415'
+      const output = solver.halfway(input)
+
+      assert.equals(output, '4')
+    })
   })
 })
