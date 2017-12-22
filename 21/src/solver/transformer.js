7390c50f01b5c664d38939380d80830ce6f28652
gundelsby <lug00ber@gmail.com>
2017-12-22 15:28:42 +0100

    Residue for day 21

--- 21/src/solver/transformer.js
+++ 21/src/solver/transformer.js
@@ -1,4 +1,4 @@
-// https://stackoverflow.com/questions/42519/how-do-you-rotate-a-two-dimensional-array
+// https://stackoverflow.com/questions/42519/how-do-you-rotate-a-two-dimensional-arraygit pu
 module.exports = {
   rotate: (buffer) => {
 
