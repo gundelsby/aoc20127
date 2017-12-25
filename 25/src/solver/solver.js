7232103ad18df186bf4b2d255bf52a1007825a12
Ola Christian Gundelsby <lug00ber@gmail.com>
2017-12-25 22:56:59 +0100

    Day 25, done and dusted

--- 25/src/solver/solver.js
+++ 25/src/solver/solver.js
@@ -1,13 +1,47 @@
 const Tape = require('./tape')
-const {Cursor} = require('./cursor')
-const states = require('./states')
+const {Cursor, directions} = require('./cursor')
+const {State, Rule} = require('./state')
+
+const states = {
+  A: new State(
+    new Rule({condition: 0, value: 1, direction: directions.RIGHT, nextState: 'B'}),
+    new Rule({condition: 1, value: 0, direction: directions.LEFT, nextState: 'C'})
+  ),
+  B: new State(
+    new Rule({condition: 0, value: 1, direction: directions.LEFT, nextState: 'A'}),
+    new Rule({condition: 1, value: 1, direction: directions.RIGHT, nextState: 'D'})
+  ),
+  C: new State(
+    new Rule({condition: 0, value: 1, direction: directions.RIGHT, nextState: 'A'}),
+    new Rule({condition: 1, value: 0, direction: directions.LEFT, nextState: 'E'})
+  ),
+  D: new State(
+    new Rule({condition: 0, value: 1, direction: directions.RIGHT, nextState: 'A'}),
+    new Rule({condition: 1, value: 0, direction: directions.RIGHT, nextState: 'B'})
+  ),
+  E: new State(
+    new Rule({condition: 0, value: 1, direction: directions.LEFT, nextState: 'F'}),
+    new Rule({condition: 1, value: 1, direction: directions.LEFT, nextState: 'C'})
+  ),
+  F: new State(
+    new Rule({condition: 0, value: 1, direction: directions.RIGHT, nextState: 'D'}),
+    new Rule({condition: 1, value: 1, direction: directions.RIGHT, nextState: 'A'})
+  )
+}
 
 module.exports = {
   part1: () => {
-    // Begin in state A.
-    // Perform a diagnostic checksum after 12919244 steps.
-    const cursor = new Cursor(new Tape())
+    let nextState = 'A'
+    const desiredSteps = 12919244
+    const tape = new Tape()
+    const cursor = new Cursor(tape)
+    for (let i = 0; i < desiredSteps; i++) {
+      nextState = states[nextState].execute(cursor)
+    }
 
+    return Object.values(tape.data).reduce((sum, entry) => {
+      return sum + entry
+    })
   },
   part2: (input) => {
     return null
--- 25/src/solver/state.js
+++ 25/src/solver/state.js
@@ -1,9 +1,28 @@
-// In state A:
-//   If the current value is 0:
-//     - Write the value 1.
-//     - Move one slot to the right.
-//     - Continue with state B.
-//   If the current value is 1:
-//     - Write the value 0.
-//     - Move one slot to the left.
-//     - Continue with state C.
+class Rule {
+  constructor ({condition, value, direction, nextState}) {
+    this.condition = condition
+    this.value = value
+    this.direction = direction
+    this.nextState = nextState
+  }
+}
+
+class State {
+  constructor (...rules) {
+    this.rules = rules
+  }
+
+  execute (cursor) {
+    const dataValue = cursor.read()
+    const rule = this.rules.find(rule => rule.condition === dataValue)
+
+    cursor.write(rule.value)
+    cursor.move(rule.direction)
+    return rule.nextState
+  }
+}
+
+module.exports = {
+  State,
+  Rule
+}
